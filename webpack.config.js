63237b80b19ebc42dab95c13e7d3e96ef7d4d8b2
vesdim2007 <vesdim2007@gmail.com>
2018-07-24 15:35:18 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require ('path');
+const webpack = require('webpack')
+const ExtractTextPlugin = require ('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if(process.env.NODE_ENV === 'test') {
+    require('dotenv').config({path: '.env.test'})
+} else if(process.env.NODE_ENV === 'development') {
+    require('dotenv').config({path: '.env.development'})
+}
+
+module.exports = (env) => {
+    const isProduction = env === 'production'
+
+    return {
+        entry: ['babel-polyfill', './src/index.js'],
+        output: {
+            path: path.resolve(__dirname, 'public/dist'),
+            filename:'bundle.js'
+        },
+        module: {
+            rules: [{
+            //babel loader   
+               loader: 'babel-loader', 
+                test: /\.js$/,
+                exclude: /node_modules/,
+                options: {
+                    plugins: ['transform-class-properties'],
+                    presets: ['env', 'react']
+                }           
+            }, 
+            //style and css loader
+            { test: /\s?.css$/,   
+            use: ExtractTextPlugin.extract({
+                use: [
+                {loader: 'css-loader', options: {sourceMap: true}}, 
+                {loader: 'sass-loader', options: {sourceMap: true}} 
+                ]           
+               })            
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin('styles.css'),
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+            })
+        ],    
+       
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            historyApiFallback: true, 
+            publicPath: '/dist/'                     
+        }    
+    }
+}
+
+
